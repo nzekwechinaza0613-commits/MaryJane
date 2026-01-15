dccbdcd8c6ca6f7ca7df5a2e827f9232f5f690c4
MaryJane <nzekwechinaza0613@gmail.com>
2026-01-15 22:23:28 +0100

    Enhance identity verification input validation

--- whoareyou.go
+++ whoareyou.go
@@ -13,56 +13,66 @@ const (
 
 func main() {
 	fmt.Println(" ")
+	fmt.Println(style("********** IDENTITY VERIFICATION ***********", "\033[1;33m", ""))
+	fmt.Println(" ")
 	fmt.Println(style("PLEASE ENTER YOUR DETAILS TO PROCEED ", "\033[1;33m",""))
 	fmt.Println(" ")
-	fmt.Println(style("=========================================", "\033[1;32m",""))
 	fmt.Print("INPUT YOUR FIRSTNAME: ")
 	start1:
 	var Firstname string
 	fmt.Scanln(&Firstname)
-	if Firstname == "" {
-		fmt.Println(style("ERROR; FIRSTNAME CANNOT BE EMPTY ❗❗❗", "\033[1;31m",""))
+	if Firstname == "" || Firstname == " " {
+		fmt.Println(" ")
+		fmt.Println(style("ERROR❗❗ FIRSTNAME CANNOT BE EMPTY OR CONTAIN SPACES", "\033[1;31m",""))
 		fmt.Println(style("TRY AGAIN", "\033[1;31m",""))
 		fmt.Println(" ")
 		fmt.Print(("PLEASE RE-ENTER YOUR FIRSTNAME: "))
 		goto start1
 	}
 	for _, char := range Firstname {
-		if (char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z') {
-			fmt.Println(style("ERROR; ONLY ALPHABETS WITHOUT SPACES ARE ALLOWED ❗❗❗", "\033[1;31m",""))
+		if (char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z') == true {
+		}else {
+			fmt.Println(" ")
+			fmt.Println(style("ERROR❗❗ ONLY ALPHABETS ARE ALLOWED", "\033[1;31m",""))
 			fmt.Println(style("TRY AGAIN", "\033[1;31m",""))
 			fmt.Println(" ")
 			fmt.Print("PLEASE RE-ENTER YOUR FIRSTNAME: ")
 			goto start1
 		}
 	}
-	fmt.Println(" ")
-	fmt.Println(style("=========================================", "\033[1;32m",""))
 	fmt.Print("INPUT YOUR LASTNAME: ")
 	start2:
 	var Lastname string
 	fmt.Scanln(&Lastname)
-	if Lastname == "" {
-		fmt.Println(style("ERROR; LASTNAME CANNOT BE EMPTY ❗❗❗", "\033[1;31m",""))
+	if Lastname == "" || Lastname == " " {
+		fmt.Println(" ")
+		fmt.Println(style("ERROR❗❗ LASTNAME CANNOT BE EMPTY OR CONTAIN SPACES", "\033[1;31m",""))
 		fmt.Println(style("TRY AGAIN", "\033[1;31m",""))
 		fmt.Println(" ")
 		fmt.Print("PLEASE RE-ENTER YOUR LASTNAME: ")
 		goto start2
 	}
 	for _, char := range Lastname {
-		if (char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z') {
-			fmt.Println(style("ERROR; ONLY ALPHABETS WITHOUT SPACES ARE ALLOWED ❗❗❗", "\033[1;31m",""))
+		if (char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z') == true {
+		}else {
+			fmt.Println(" ")
+			fmt.Println(style("ERROR❗❗ ONLY ALPHABETS ARE ALLOWED", "\033[1;31m",""))
 			fmt.Println(style("TRY AGAIN", "\033[1;31m",""))
 			fmt.Println(" ")
 			fmt.Print("PLEASE RE-ENTER YOUR LASTNAME: ")
 			goto start2
 		}
 	}
-	fmt.Println(style("ADMIN PASSWORD REQUIRED TO PROCEED", "\033[1;33m",""))
+	fmt.Println(" ")
+	fmt.Printf(style("HELLO %s %s! YOU ARE VERIFIED\n", "\033[1;32m",""), Firstname, Lastname)
+	fmt.Println(" ")
+	fmt.Println(style("=========================================", "\033[1;32m",""))
+	fmt.Println(" ")
+	fmt.Println(style("TO ENTER VIP SECTION, YOU NEED TO INPUT AN ADMIN PASSWORD", "\033[1;33m",""))
 	fmt.Println(" ")
 	var Password string
 	for attempt := 1; attempt <= maxretry; attempt++ {
-	fmt.Println(style("PLEASE ENTER YOUR 6-DIGIT PASSWORD: ", "\033[1;33m",""))
+	fmt.Println(style("PLEASE ENTER YOUR 6-DIGIT ADMIN PASSWORD: ", "\033[1;33m",""))
 	fmt.Println("YOU HAVE ", maxretry-attempt+1, "ATTEMPTS LEFT")
 	fmt.Scanln(&Password)
 	if len(Password) != 6 {
@@ -86,10 +96,12 @@ func main() {
 			return
 		} 
 	fmt.Println(style("PASSWORD ACCEPTED ✅", "\033[1;32m",""))
+	fmt.Println(style("VIP STATUS CONFIRMED ✅", "\033[1;32m",""))
 	fmt.Println(" ")
-	fmt.Printf(style("SPECIAL GUEST DETECTED! Hello %s %s!,WELCOME TO 𝗛 𝗘 𝗔 𝗩 𝗘 𝗡 👑🕊️ \n", "\033[1;32m",""), Firstname, Lastname)
-	fmt.Println(style("YOU HAVE BEEN GRANTED ACCESS DO ENJOY YOUR STAY HERE💫", "\033[1;32m",""))
-	fmt.Println("==========================================")
+	fmt.Printf(style("MY CHEIF %s %s!\n", "\033[1;32m",""), Firstname, Lastname)
+	fmt.Println(style("WELCOME TO 𝗛 𝗘 𝗔 𝗩 𝗘 𝗡 👑🕊️ \n", "\033[1;32m",""))
+	fmt.Println(style("THE SYSTEM BOWS TO YOU RESPECTFULLY OF COURSE", "\033[1;32m",""))
+	fmt.Println(style("|></></></></></></></></></></></></></></></></></></></></><|", "\033[1;30m",""))
 	return
 		}
 	}
